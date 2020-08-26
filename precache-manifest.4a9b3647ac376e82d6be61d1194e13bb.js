2386ac4d02ff2bd539f79b6828dfcf6ec93206cc
kihoonkim <koreakihoon@gmail.com>
2020-08-26 17:32:17 +0900

    Updates

---
+++ precache-manifest.4a9b3647ac376e82d6be61d1194e13bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0f8085db2f57a6594f410d02c4f716",
+    "url": "/invitation_troyanna830/index.html"
+  },
+  {
+    "revision": "999fd467e626fae9efc6",
+    "url": "/invitation_troyanna830/static/css/main.2e4519cd.chunk.css"
+  },
+  {
+    "revision": "084ab179310db407952d",
+    "url": "/invitation_troyanna830/static/js/2.e90a2c7a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/invitation_troyanna830/static/js/2.e90a2c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999fd467e626fae9efc6",
+    "url": "/invitation_troyanna830/static/js/main.3242ab87.chunk.js"
+  },
+  {
+    "revision": "ef2f17fb54d80e956087",
+    "url": "/invitation_troyanna830/static/js/runtime-main.7a8de225.js"
+  },
+  {
+    "revision": "4c17eff8051167d9bf09aa71e3c2cdf5",
+    "url": "/invitation_troyanna830/static/media/main.4c17eff8.jpg"
+  }
+]);
